203f59ee3504711b7d38477a2609fc3ff53b3bca
Raymond van der zanden <Raymond.van.der.zanden@rabobank.nl>
2020-05-31 01:17:34 +0200

    first basic calculation of resources needed

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,4 +1,8 @@
-print("Starting to make a coffee\nGrinding coffee beans"
-      "\nBoiling water\nMixing boiled water with crushed coffee beans"
-      "\nPouring coffee into the cup\nPouring some milk into the cup"
-      "\nCoffee is ready!")
+total_cups = int(input("Write how many cups of coffee you will need: "))
+water_per_cup = 200
+milk_per_cup = 50
+beans_per_cup = 15
+print(f"For {total_cups} cups of coffee you will need: \n")
+print(f"{water_per_cup * total_cups} ml of water")
+print(f"{milk_per_cup * total_cups} ml of milk")
+print(f"{beans_per_cup * total_cups} g of coffee beans")
