f14ff12b174b95bc71600e24d4065aa9805de1e2
Raymond van der zanden <Raymond.van.der.zanden@rabobank.nl>
2020-05-31 13:35:13 +0200

    check coordinate Chaining comparison operators

--- Problems/Coordinates/task.py
+++ Problems/Coordinates/task.py
@@ -3,9 +3,9 @@ y = float(input())
 
 if x < 0 and y < 0:
     print("III")
-elif x < 0 and y > 0:
+elif x < 0 < y:
     print("II")
-elif x > 0 and y > 0:
+elif x > 0 < y:
     print("I")
 else:
     print("IV")
